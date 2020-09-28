f0c5cca9735e49ef7e438cc382f0e46f2fcebf85
Aubrey Portwood <code@aubreypwd.com>
2020-09-28 14:13:59 -0600

    Spam the overrides to ensure that it only formats *.js, and *.jsx files.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = {
-	overrides: {
-		files: [ '*.js', '*.jsx' ],
-		options: {
-			...require( '@wordpress/prettier-config' ),
-			printWidth: 99000,
-			bracketSpacing: true,
+	overrides: [
+		{
+			files: [ '*.js', '*.jsx' ],
+			options: {
+				...require( '@wordpress/prettier-config' ),
+				printWidth: 99000,
+				bracketSpacing: true,
+			},
 		},
-	},
+	],
 };
