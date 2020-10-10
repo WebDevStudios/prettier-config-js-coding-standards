9e7b230c4e35a4d20c128ee628a5ccaf48c22eaa
Aubrey Portwood <code@aubreypwd.com>
2020-10-09 18:40:08 -0600

    Add multiline array example.

---
+++ examples/multiline-arrays.js
@@ -0,0 +1,41 @@
+/**
+ * Multiline Arrays
+ *
+ * @purpose We don't want prettier to condense multiline arrays.
+ *
+ * @since 10/9/20
+ * @author Aubrey Portwood <aubrey@webdevstudios.com>
+ */
+
+const itema = 1;
+const itemb = 2;
+
+// Right now prettier will condense these.
+const a = [
+	itema,
+	itemb,
+	itema,
+	itemb,
+	itema,
+	itemb,
+	itema,
+	itemb,
+	itema,
+	itemb,
+	itema,
+	itemb,
+	itema,
+	itemb,
+	itema,
+	itemb,
+	itema,
+	itemb,
+	itema,
+	itemb,
+	itema,
+	itemb,
+	itema,
+	itemb,
+];
+
+return a;
